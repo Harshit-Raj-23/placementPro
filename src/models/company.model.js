80664d14af271459b3f4fab4bab19eca74cb7b53
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2026-01-20 22:47:38 +0530

    Company Model Created with Validation

---
+++ src/models/company.model.js
@@ -0,0 +1,69 @@
+import mongoose, { Schema } from "mongoose";
+import validator from "validator";
+import ApiError from "../utils/ApiError.js";
+
+const companySchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    website: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new ApiError(400, "Invalid website URL");
+        }
+      },
+    },
+    logo: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new ApiError(400, "Invalid logo URL");
+        }
+      },
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    industry: {
+      type: String,
+      required: true,
+    },
+    foundedYear: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+companySchema.pre("save", async function (next) {
+  if (!this.isModified("website")) return next();
+
+  if (!this.website.match(/^https?:\/\//)) {
+    this.website = `https://${this.website}`;
+  }
+});
+
+const Company = mongoose.model("Company", companySchema);
+
+export default Company;
